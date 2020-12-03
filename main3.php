e1b34ac4e33ebfcca2ea0ca02a854259078946f8
DHANYA1997 <dhanyavarghese00@gmail.com>
2020-12-03 17:14:43 +0530

    navigation code updated

--- main3.php
+++ main3.php
@@ -11,10 +11,10 @@
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <ul class="navbar-nav">
           <li class="nav-item active">
-            <a class="nav-link" href="index.html">Login</a>
+            <a class="nav-link" href="index.php">Login</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="main3.html">Home</a>
+            <a class="nav-link" href="main3.php">Home</a>
           </li>
         </ul>
       </nav>
