0b210cfba660d6291adad61a65efd822b90a59e0
DHANYA1997 <dhanyavarghese00@gmail.com>
2020-12-03 17:10:53 +0530

    background created

--- index.php
+++ index.php
@@ -8,6 +8,16 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body class="bgcolor">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="main3.php">Home</a>
+          </li>
+        </ul>
+      </nav>
     <div class="container">
         <div class="row">
             <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
